bb2832e18afd718375f7f750aaa241f98a808508
Mihajlo Colic <colicmihajlo@gmail.com>
2025-11-30 22:45:28 +0100

    Added local alternative for fetching objects, if backend is down

--- scripts/FetchCategories.js
+++ scripts/FetchCategories.js
@@ -12,9 +12,40 @@ function filterProducts(categoryId) {
     });
 }
 
+function fetchLocalCategories() {
+    fetch('./scripts/OfflineCategories.json')
+        .then(res => {
+            if(!res.ok) {
+                throw new Error(`HTTP ERROR: ${res.status}`);
+            }
+            return res.json();
+        })
+        .then(data => {
+             data.forEach(category => {
+                fetchedCategories.push(category);
+            });
+            fetchedCategories.forEach(category => {
+            const dropdownItem = document.createElement("a");
+            dropdownItem.textContent = category.Name;
+            dropdownItem.dataset.categoryId = category.Id;
+            dropdownItem.href = "#";
+            categoryDropdown.appendChild(dropdownItem);
+
+            categoryDropdown.addEventListener("click", (e) => {
+                e.preventDefault();
+                const categoryId = e.target.dataset.categoryId;
+                filterProducts(categoryId);
+            });
+        });
+        })
+        .catch(error => console.error('Error loading JSON:', error));
+    return
+}
+
 fetch("https://ecommerce-api20251105080905-b7c5d8gbazahhhdw.canadacentral-01.azurewebsites.net/api/category").then((res) => {
     if(!res.ok) {
         throw new Error(`HTTP ERROR: ${res.status}`);
+        
     }
     return res.json()
     
@@ -35,4 +66,8 @@ fetch("https://ecommerce-api20251105080905-b7c5d8gbazahhhdw.canadacentral-01.azu
             filterProducts(categoryId);
         });
     });
+})
+.catch(error => {
+    console.error('Error fetching categories:', error);
+    fetchLocalCategories();
 });
--- scripts/FetchProducts.js
+++ scripts/FetchProducts.js
@@ -1,13 +1,32 @@
+function fetchLocalProducts() {
+    fetch('./scripts/OfflineProducts.json')
+        .then(res => {
+            if(!res.ok) {
+                throw new Error(`HTTP ERROR: ${res.status}`);
+            }
+            return res.json();
+        })
+        .then((data) => {
+            displayProducts(data);
+        })
+        .catch(error => console.error('Error loading JSON:', error));
+}
+
 function fetchProducts() {
-    fetch("https://ecommerce-api20251105080905-b7c5d8gbazahhhdw.canadacentral-01.azurewebsites.net/api/product").then((res) => {
-        if(!res.ok) {
-            throw new Error(`HTTP ERROR: ${res.status}`);
-        }
-        return res.json();
-    
-    }).then((data) => {
-        displayProducts(data);
-    });
+    fetch("https://ecommerce-api20251105080905-b7c5d8gbazahhhdw.canadacentral-01.azurewebsites.net/api/product")
+        .then((res) => {
+            if(!res.ok) {
+                throw new Error(`HTTP ERROR: ${res.status}`);
+            }
+            return res.json();
+        })
+        .then((data) => {
+            displayProducts(data);
+        })
+        .catch(error => {
+            console.error('Error fetching products:', error);
+            fetchLocalProducts();
+        });
 }
 
 fetchProducts();
